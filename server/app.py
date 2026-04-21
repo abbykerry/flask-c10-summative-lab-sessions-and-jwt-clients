ed7154d22c06db06850c28e0ad7e446557af2a65
Abigael Ongwae <abbykerry17@gmail.com>
2026-04-21 13:25:15 +0300

    initialized backend server and installed all dependencies + laid out structure

---
+++ server/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from config import db, migrate, bcrypt, jwt
+
+app = Flask(__name__)
+
+# basic config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret-key'  # change later if needed
+
+# init extensions
+db.init_app(app)
+migrate.init_app(app, db)
+bcrypt.init_app(app)
+jwt.init_app(app)
+
+# simple route (just to test)
+@app.route('/')
+def home():
+    return {"message": "API is running"}
